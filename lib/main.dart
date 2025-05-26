fb5b335d4b714e3de47c16cfb1043da544988c28
Juan Agustin Apaza <juanagustin7887@gmail.com>
2025-05-26 14:43:40 -0400

    agregue una imagen

--- lib/main.dart
+++ lib/main.dart
@@ -105,6 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
+            Image.asset('assets/images/papa.jpg', width: 150),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
